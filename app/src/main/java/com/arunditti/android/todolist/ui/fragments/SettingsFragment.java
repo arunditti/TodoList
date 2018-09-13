eb0f663636184878220bb53ba9c09c7d274d390b
arunditti <arunditti@gmail.com>
2018-09-12 23:17:14 -0400

    Add widget

---
+++ app/src/main/java/com/arunditti/android/todolist/ui/fragments/SettingsFragment.java
@@ -0,0 +1,8 @@
+package com.arunditti.android.todolist.ui.fragments;
+
+/**
+ * Created by arunditti on 9/9/18.
+ */
+
+public class SettingsFragment {
+}
---
+++ app/src/main/java/com/arunditti/android/todolist/widget/TodoListWidgetService.java
@@ -0,0 +1,8 @@
+package com.arunditti.android.todolist.widget;
+
+/**
+ * Created by arunditti on 9/12/18.
+ */
+
+public class TodoListWidgetService {
+}
